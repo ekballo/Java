56cc1d1dc93a4f1de890d3051988e2e4b9a21970
Jefferson Garcia <ekball007@outlook.com>
2022-09-19 06:20:56 -0300

    Token.Agoravai2.0

---
+++ Token/autentica-senha/src/main/java/agoravai20/autenticasenha/AutenticaSenhaApplication.java
@@ -0,0 +1,13 @@
+package agoravai20.autenticasenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticaSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticaSenhaApplication.class, args);
+	}
+
+}
---
+++ Token/autentica-senha/src/test/java/agoravai20/autenticasenha/AutenticaSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package agoravai20.autenticasenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticaSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
